fb2fb3f1aa83867a6c184abb80a44fe73fc03233
mayco <maycon103>
2023-10-11 21:47:45 -0400

    atualização de dependências

--- recycleview&expandable/src/main/java/com/android/msm/recycleviewexpandable/interfaces/JsonConvert.java
+++
@@ -1,7 +0,0 @@
-package com.example.expandablerecyclerview.interfaces;
-
-import com.google.gson.JsonArray;
-
-public interface JsonConvert {
-    public  void asJsonArray(JsonArray jsonArray);
-}
--- recycleview&expandable/src/main/java/com/android/msm/recycleviewexpandable/interfaces/RecyclerViewOnCheckBox.java
+++
@@ -1,9 +0,0 @@
-package com.example.expandablerecyclerview.interfaces;
-
-import android.widget.CheckBox;
-
-import com.google.gson.JsonArray;
-public interface RecyclerViewOnCheckBox {
-
-    public void OnCheckBox(CheckBox checkBox, JsonArray json, int position);
-}
