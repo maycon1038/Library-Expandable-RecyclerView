7be6e6a769b3546fadda5de13fdf46016c4e9e4f
maycon <maycon1038@gmail.com>
2023-10-16 13:39:53 -0400

    atualização de dependências

--- recycleview&expandable/src/main/java/com/android/msm/recycleviewexpandable/interfaces/RecyclerViewOnCheckBox.java
+++
@@ -1,9 +0,0 @@
-package com.android.msm.recycleviewexpandable.interfaces;
-
-import android.widget.CheckBox;
-
-import com.google.gson.JsonArray;
-public interface RecyclerViewOnCheckBox {
-
-    public void OnCheckBox(CheckBox checkBox, JsonArray json, int position);
-}
