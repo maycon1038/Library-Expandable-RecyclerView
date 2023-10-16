e9214f380d3522ce3aeb836b1b77d157a7ce8561
maycon <maycon1038@gmail.com>
2023-10-16 10:59:59 -0400

    atualização de dependências

--- app/src/main/java/com/android/msm/exemplo/animais.java
+++ app/src/main/java/com/android/msm/exemplo/animais.java
@@ -1,9 +1,24 @@
 package com.android.msm.exemplo;
 
-import lombok.Data;
-@Data
 public class animais {
     private int ranking;
+
+    public int getRanking() {
+        return ranking;
+    }
+
+    public void setRanking(int ranking) {
+        this.ranking = ranking;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
     private  String raca;
     public animais(int rancking, String raca) {
         ranking = rancking;
